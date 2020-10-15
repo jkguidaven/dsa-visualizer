030945237ec73ed0e5266b08540004e1bb26ec81
James Kenneth A. Guidaven <jkguidaven@gmail.com>
2020-10-15 19:28:30 +0800

    Update material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutComponent } from './common/layout/layout.component';
 
 // Angular Material
-import {
-  MatSidenavModule,
-  MatIconModule,
-  MatToolbarModule,
-  MatDividerModule,
-  MatListModule,
-  MatButtonModule,
-  MatSelectModule,
-  MatSliderModule,
-  MatCheckboxModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 import { PageHeaderComponent } from './common/layout/page-header/page-header.component';
